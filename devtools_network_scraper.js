fe5d489f0e021aefbb645a387c6f4c2968e6820e
Luis Eduardo <78921021+Dahsleman@users.noreply.github.com>
2023-07-13 16:39:24 -0300

    latest update

--- devtools_network_scraper.js
+++ devtools_network_scraper.js
@@ -1,42 +1,43 @@
 const puppeteer = require('puppeteer');
 
-async function scrapeNetworkDevTools() {
-  // Launch a headless Chrome browser
+async function scrapeNetworkRequests() {
   const browser = await puppeteer.launch();
-
-  // Create a new page
   const page = await browser.newPage();
 
   // Enable request interception
   await page.setRequestInterception(true);
 
-  // Create an array to store intercepted requests
-  const interceptedRequests = [];
-
-  // Listen for request events
+  try{
+    // Listen to the request event
   page.on('request', (request) => {
-    interceptedRequests.push(request);
+    if (request.method() == 'POST' && request.url() == 'https://services.rappi.com.br/api/pns-global-search-api/v1/unified-recent-top-searches') {
+      // console.log('Request URL:', request.url());
+      // console.log('Request Method:', request.method());
+      // console.log('Request Headers:', request.headers());
+      // console.log('--------------------'); 
+      const headersJson = JSON.stringify(request.headers(), null, 2);
+      const headers = JSON.parse(headersJson);
+      const authorization = headers.authorization;
+      console.log(authorization); 
+      process.exit();
+    }
     request.continue();
   });
+  // Increase the navigation timeout to 120 seconds (60000 milliseconds)
+  // await page.setDefaultNavigationTimeout(3000);
 
-  // Navigate to the desired URL
+  // Navigate to the Rappi website
   await page.goto('https://www.rappi.com.br/');
 
-  // Perform other interactions or wait for the page to load as needed
-
-  // Print the intercepted requests
-  for (const request of interceptedRequests) {
-    if (request.url() == 'https://services.rappi.com.br/api/pns-global-search-api/v1/unified-recent-top-searches' && request.method() == 'POST' ) {
-        const headersJson = JSON.stringify(request.headers(), null, 2);
-        const headers = JSON.parse(headersJson);
-        const authorization = headers.authorization;
-        console.log(authorization);
-    }
-  }
+  // Wait for the page to load
+  await page.waitForNavigation();
 
   // Close the browser
   await browser.close();
+
+  } catch (error){
+    console.error('An error occurred:', error);
+  }
 }
 
-// Run the scraping function
-scrapeNetworkDevTools();
+scrapeNetworkRequests();
